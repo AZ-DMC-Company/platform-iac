4db16c8b8e8064032f1d757ed0ab494b3324940b
unknown <ricvera@favoritafc.com>
2026-03-09 22:09:25 -0500

    Change name StorageCenter

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-terraform-state"
-    storage_account_name = "tfstateplatformxyz123"
+    storage_account_name = "tfstateplataformxyz123"
     container_name       = "tfstate"
     key                  = "infrastructure.tfstate"
   }
