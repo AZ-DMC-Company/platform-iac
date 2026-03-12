4e8dfc5d2e98b5d49e5a7770095ab603e46d2648
unknown <ricvera@favoritafc.com>
2026-03-11 22:27:10 -0500

    Remove destroy (dangerous zone)

--- bootstrap/variables.tf
+++ bootstrap/variables.tf
@@ -7,17 +7,17 @@ variable "location" {
 variable "resource_group_name" {
   description = "Resource group for Terraform state"
   type        = string
-  default     = "rg-terraform-state"
+  default     = "rg-ricvera-dev-eastus-001"
 }
 
 variable "storage_account_name" {
   description = "Storage account for tfstate"
   type        = string
-  default     = "tfstateplataformxyz123"
+  default     = "tfstatericveradev01"
 }
 
 variable "container_name" {
   description = "Container for Terraform state"
   type        = string
-  default     = "tfstate"
+  default     = "tfstate-ricvera-dev"
 }
