8e3a159a6711ba8184373b6526b815a2fb8cfc36
unknown <ricvera@favoritafc.com>
2026-03-14 20:40:09 -0500

    Add new version of azzureprovider for Azure Storage

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -1,8 +1,12 @@
-provider "azurerm" {
-  features {}
-}
 terraform {
   required_version = ">= 1.5"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=5.0.0"
+    }
+  }
 
   backend "azurerm" {
     resource_group_name  = "orders-rg-tfstate-dev-eus-01"
@@ -10,4 +14,8 @@ terraform {
     container_name       = "tfstate"
     key                  = "app-infrastructure.tfstate"
   }
+}
+
+provider "azurerm" {
+  features {}
 }
