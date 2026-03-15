ac1c49d6cbe8d6ee6f60c49e17d987cddb10f889
unknown <ricvera@favoritafc.com>
2026-03-14 19:25:49 -0500

    Include Grafana CA

--- infrastructure/grafana.tf
+++ infrastructure/grafana.tf
@@ -1,1 +1,36 @@
-# Placeholder Grafana
+resource "azurerm_container_app" "grafana" {
+  name                         = "${var.workload}-grafana-${var.env}-01"
+  resource_group_name          = azurerm_resource_group.rg_app.name
+  container_app_environment_id = azurerm_container_app_environment.cae.id
+  revision_mode                = "Single"
+
+  ingress {
+    external_enabled = true
+    target_port      = 3000
+    transport        = "auto"
+
+    traffic_weight {
+      latest_revision = true
+      percentage      = 100
+    }    
+  }
+
+  template {
+    container {
+      name   = "grafana"
+      image  = "grafana/grafana:latest"
+      cpu    = 0.25
+      memory = "0.5Gi"
+
+      # Variables de entorno básicas para dev
+      env {
+        name  = "GF_SECURITY_ADMIN_USER"
+        value = "admin"
+      }
+      env {
+        name  = "GF_SECURITY_ADMIN_PASSWORD"
+        value = "admin123"
+      }
+    }
+  }
+}
