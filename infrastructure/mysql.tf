f19c5d973b6e5a943129e3f981758ca6c12041c6
unknown <ricvera@favoritafc.com>
2026-03-15 00:03:15 -0500

    Fix by myself

--- infrastructure/mysql.tf
+++ infrastructure/mysql.tf
@@ -1,23 +1,24 @@
-# mysql.tf - compatible con azurerm 3.*
 resource "azurerm_mysql_server" "grafana_db" {
   name                = "${var.workload}-grafana-db-${var.env}"
-  resource_group_name = azurerm_resource_group.rg_app.name
   location            = azurerm_resource_group.rg_app.location
+  resource_group_name = azurerm_resource_group.rg_app.name
+
   administrator_login = "grafanaadmin"
   administrator_login_password = var.grafana_mysql_password
-  version             = "5.7"
 
   sku_name = "B_Gen5_1"   # Gratis / mínima B-series
-  storage_profile {
-    storage_mb            = 5120
-    backup_retention_days = 7
-    geo_redundant_backup  = "Disabled"
-  }
+  version             = "5.7"
+  storage_mb            = 5120
 
-  ssl_enforcement = "Enabled"
+  auto_grow_enabled                 = true
+  backup_retention_days             = 7
+  geo_redundant_backup_enabled      = false
+  infrastructure_encryption_enabled = false
+  public_network_access_enabled     = true
+  ssl_enforcement_enabled           = true
+  ssl_minimal_tls_version_enforced  = "TLS1_2"
 }
 
-# Base de datos Grafana
 resource "azurerm_mysql_database" "grafana_db_name" {
   name                = "grafana"
   resource_group_name = azurerm_resource_group.rg_app.name
